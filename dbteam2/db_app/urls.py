94a8c719b02b025645d40b713b34a35589f5a727
Rafael Martins <36113090+rafaelbertomartins@users.noreply.github.com>
2019-07-02 11:36:14 +0100

    Add files via upload

--- dbteam2/db_app/urls.py
+++ dbteam2/db_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('search_route/', views.search_route, name='search_route'),
 ]
--- dbteam2/db_app/views.py
+++ dbteam2/db_app/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render
+import json, pymysql, sys, requests
+from django.http import HttpResponse
 
 # Create your views here.
 def main_page(request):
 
-    import sys
-    import pymysql
-    import json
+    # host='127.0.0.1'
+    # user = 'root'
+    # password = 'A0206304131z'
+    # db = 'db_data'
+    #
+    # try:
+    #     con = pymysql.connect(host=host,user=user,password=password,db=db, use_unicode=True, charset='utf8')
+    # except Exception as e:
+    #     sys.exit(e)
+    #
+    # cur = con.cursor()
+    # cur.execute("select stop_lat, stop_lon, stopid_updated, stop_name from db_data.stops_updated where stop_name='Belfield, University College Dublin' or stop_name='Trinity College';")
+    # data = cur.fetchall()
+    #
+    # return render(request, 'db_app/main_page.html', {'data': json.dumps(data)})
+    return render(request, 'db_app/main_page_styled.html', {'apikey': 'AIzaSyCM9nXFgqm8JbVlEYRAiPv6WTUFGSvyTBU'})
 
-    host='127.0.0.1'
-    user = 'root'
-    password = 'A0206304131z'
-    db = 'db_data'
+def search_route(request):
+    origin = request.POST['origin']
+    destination = request.POST['destination']
 
-    try:
-        con = pymysql.connect(host=host,user=user,password=password,db=db, use_unicode=True, charset='utf8')
-    except Exception as e:
-        sys.exit(e)
+    googleRequest = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&mode=transit&key=AIzaSyCM9nXFgqm8JbVlEYRAiPv6WTUFGSvyTBU')
 
-    cur = con.cursor()
-    cur.execute("select stop_lat, stop_lon, stopid_updated, stop_name from db_data.stops_updated where stop_name='Belfield, University College Dublin' or stop_name='Trinity College';")
-    data = cur.fetchall()
+    print(googleRequest)
 
-    return render(request, 'db_app/main_page.html', {'data': json.dumps(data)})
+    return HttpResponse(googleRequest)
