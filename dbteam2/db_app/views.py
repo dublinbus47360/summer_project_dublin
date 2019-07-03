ea985442a8d2b686b83446f36c76c7609d64c5fe
Rafael Martins <36113090+rafaelbertomartins@users.noreply.github.com>
2019-07-03 19:02:20 +0100

    basic front-end

--- dbteam2/db_app/views.py
+++ dbteam2/db_app/views.py
@@ -5,22 +5,27 @@ from django.http import HttpResponse
 # Create your views here.
 def main_page(request):
 
-    # host='127.0.0.1'
-    # user = 'root'
-    # password = 'A0206304131z'
-    # db = 'db_data'
-    #
-    # try:
-    #     con = pymysql.connect(host=host,user=user,password=password,db=db, use_unicode=True, charset='utf8')
-    # except Exception as e:
-    #     sys.exit(e)
-    #
-    # cur = con.cursor()
-    # cur.execute("select stop_lat, stop_lon, stopid_updated, stop_name from db_data.stops_updated where stop_name='Belfield, University College Dublin' or stop_name='Trinity College';")
-    # data = cur.fetchall()
-    #
+    host='127.0.0.1'
+    user = 'root'
+    password = 'A0206304131z'
+    db = 'db_data'
+
+    try:
+        con = pymysql.connect(host=host,user=user,password=password,db=db, use_unicode=True, charset='utf8')
+    except Exception as e:
+        sys.exit(e)
+
+    cur = con.cursor()
+    cur.execute("SELECT distinct bus_line FROM db_data.stoptimes_filtered order by bus_line;")
+    data = cur.fetchall()
+
+    bus_lines = []
+    for i in data:
+        bus_lines.append(i[0])
+    # print(bus_lines)
+
     # return render(request, 'db_app/main_page.html', {'data': json.dumps(data)})
-    return render(request, 'db_app/main_page_styled.html', {'apikey': 'AIzaSyCM9nXFgqm8JbVlEYRAiPv6WTUFGSvyTBU'})
+    return render(request, 'db_app/main_page_styled.html', {'apikey': 'AIzaSyCM9nXFgqm8JbVlEYRAiPv6WTUFGSvyTBU', 'bus_lines': json.dumps(bus_lines)})
 
 def search_route(request):
     origin = request.POST['origin']
